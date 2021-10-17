dbee679d4cfee90500bf95dd8190cb4c3c0093ab
nemo1999 <tiny.nemo.fish@gmail.com>
2021-10-18 02:37:06 +0800

    Add comment

--- plot_thread.py
+++ plot_thread.py
@@ -22,13 +22,22 @@ def test(mode='single_thread', arr_size=10000000, num_thread=None, num_proc=None
     return total_usecs / num_samples
 
 if __name__ == '__main__':
-    # execute experiments
-    mode = 'multi_thread'
+
+    # start of set parameters -------------------------------------------------------------------
+    # number of experiments to run for each test (result is averaged)
     num_sample = 5
+     # number of value to test for array_size
     num_x_vals = 11
+    # start of range of array_size
     x_start = 0
+    # end of range of array_size
     x_end = 50000000
+    # number of different thread numbers to try  (try powers of 2 start from 2**0)
     num_thread_setting = 5
+    # end of set parameters ------------------------------------------------------------------
+
+    # execute experiments
+    mode = 'multi_thread'  # don't change this, this script for multi-thread
     times = [[] for _  in range(num_thread_setting)]
     xs = []
     for arr_size in np.linspace(x_start, x_end, num_x_vals):
@@ -42,13 +51,13 @@ if __name__ == '__main__':
             print(f'\tthreads: {num_thread:4.1f}, time: {t:10.1f} (uS)')
 
     # plot figure
-    fig, ax = plt.subplots(figsize=(6, 5.5))
+    fig, ax = plt.subplots(figsize=(7, 5))
     ax.set_title('Multi_thread Experiments')
     for i in range(num_thread_setting):
         percent = (i/num_thread_setting)
-        r = 0.0 + 0.7 * percent
+        r = 0.0 + 0.9 * percent
         g = 0.1
-        b = 0.8 - 0.8 * percent
+        b = 0.8 - 0.9 * percent
         color = (r, g, b, 1.0)
         ax.plot(xs, times[i], color=color , label=f'{2**i} threads')
 
